b4a024adb3481a69a5ba03be4a0212d4114f1909
Christine Hong <christinehong802@gmail.com>
2015-04-04 20:23:13 -0400

    Jose's make host pair list

---
+++ makeHostPairList.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python
+
+# This program reads a file of tokens, one per line, and outputs
+# all unique pairs (i.e., ordering does not matter).
+
+import random
+import sys
+
+DBG = False
+RAND = False
+
+if __name__ == "__main__":
+
+    # Flags and arguments.
+    argv = []
+    for a in sys.argv:
+        if a.startswith('--'):
+            DBG = DBG or a == '--debug'
+            RAND = RAND or a == '--rand'
+        else:
+            argv.append(a)
+    if len(argv) < 2:
+        sys.exit('usage: ' + argv[0] + ' [--debug] [--rand] <tokens file>')
+
+    # Read the tokens.
+    tokens = [token.strip() for token in open(argv[1])]
+
+    # Enumerate the unique pairs.
+    pairs = []
+    for i, first in enumerate(tokens):
+        for x in range(i + 1, len(tokens)):
+            second = tokens[x]
+            if RAND and random.randint(0, 1) == 1:
+                pairs.append(second + " " + first)
+            else:
+                pairs.append(first + " " + second)
+
+    # Sort and output the pairs.
+    pairs.sort()
+    for p in pairs:
+        print p
