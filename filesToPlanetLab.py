8d8b10810e24e57d70230bb0520936b764fcbb85
Sage Price <sage.price@yale.edu>
2015-04-04 21:22:18 -0400

    copy local crux folder to planetlab nodes

---
+++ filesToPlanetLab.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+#download files onto planetlab node using github
+#e.g. python filesToPlanetLab.py -nodes nodes.txt
+
+import sys
+import argparse
+import subprocess
+import os
+
+#read file 
+parser = argparse.ArgumentParser()
+parser.add_argument('-nodes')
+args = parser.parse_args()
+nodeFile = args.nodes
+nodes = []
+with open(nodeFile, 'r') as f:
+    for line in f:
+        nodes.append(line.rstrip('\n'))
+
+#call ssh
+for node in nodes:
+    p = subprocess.Popen(["scp","-r", "../crux", "yale_dissent@" + node + ":/home/yale_dissent"])
+    sts = os.waitpid(p.pid, 0)
