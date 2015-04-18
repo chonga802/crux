1c0666b8fa06432eeb64b06e38e29d8c83593192
Christine Hong <christinehong802@gmail.com>
2015-04-18 16:15:32 -0400

    filesToPlanetLab now accepts any folder/file

--- filesToPlanetLab.py
+++ filesToPlanetLab.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #download files onto planetlab node using scp
-#e.g. python filesToPlanetLab.py -nodes nodes.txt
+#e.g. python filesToPlanetLab.py -item <path-name> -nodes nodes.txt
 
 import sys
 import argparse
@@ -10,8 +10,10 @@ import os
 #read file 
 parser = argparse.ArgumentParser()
 parser.add_argument('-nodes')
+parser.add_argument('-item')
 args = parser.parse_args()
 nodeFile = args.nodes
+item = args.item
 nodes = []
 f = open(nodeFile, 'r')
 for line in f:
@@ -20,5 +22,5 @@ f.close()
 
 #call ssh
 for node in nodes:
-    p = subprocess.Popen(["scp","-r", "../crux", "yale_dissent@" + node + ":/home/yale_dissent"])
+    p = subprocess.Popen(["scp","-r", item, "yale_dissent@" + node + ":/home/yale_dissent"])
     sts = os.waitpid(p.pid, 0)
