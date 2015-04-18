56bec2ef6ba21fbb53f5b006ba17ff87790e74c0
Christine Hong <christinehong802@gmail.com>
2015-04-18 16:47:09 -0400

    remove dead nodes

---
+++ removeDeadNodes.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+#remove dead nodes from nodes.txt, save them in aliveNodes.txt
+#e.g. removeDeadNodes.py -nodes nodes.txt
+
+import sys
+import argparse
+import subprocess
+import socket
+
+#read file 
+parser = argparse.ArgumentParser()
+parser.add_argument('-nodes')
+args = parser.parse_args()
+nodeFile = args.nodes
+#put nodes into array
+nodes = []
+f = open(nodeFile, 'r')
+for line in f:
+    nodes.append(line.rstrip('\n'))
+f.close()
+#ping each node, if no response after 5 seconds, don't add node to aliveNodees
+aliveNodes = []
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+for node in nodes:
+    try:
+        s.connect((node, 22))
+        aliveNode.append(node)
+    except socket.error:
+#save bunch in aliveNodes.txt
+fout = open("aliveNodes.txt", 'w')
+for node in aliveNodes:
+    fout.write(node +  "\n")
+fout.close()
+
+
