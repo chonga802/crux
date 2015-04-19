344b124d22879cc2174e9a2ee80d3c4d85f8e153
Kojiro Murase <kojiro.murase@yale.edu>
2015-04-19 15:38:24 -0400

    Added inputs

--- Trader/trader.py
+++ Trader/trader.py
@@ -11,19 +11,24 @@ import subprocess
 import time
 import os
 import socket
+import sys
 
 # Construction of rings A
 # Given list of ports, makes Mongo databases on those ports centered on local host
-def build_rings (mpath, portlist):
+def build_rings (mpath, host, portlist):
+
+	host = host.split(".")[0]
 
 	for port in portlist:
 
-		if not os.path.exists(mpath+'/data'+port):
-			os.makedirs(mpath+'/data'+port)
+		mypath = mpath+'/data_at_'+host+"_"+port
+
+		if not os.path.exists(mypath):
+			os.makedirs(mypath)
 
 		# out = subprocess.check_output(["echo", "Hello World!"])
 
-		p = subprocess.Popen([mpath + '/mongod', '--dbpath', mpath+'/data'+port, '--port', port], stdout=subprocess.PIPE)
+		p = subprocess.Popen([mpath + '/mongod', '--dbpath', mypath, '--port', port], stdout=subprocess.PIPE)
 		# result = p.communicate()[0]
 		# print "STARTED:" + result
 
@@ -31,14 +36,18 @@ def build_rings (mpath, portlist):
 
 # Destruction of rings A
 # Given list of ports, shuts down Mongo databases on those ports centered on local host
-def kill_rings (mpath, portlist):
+def kill_rings (mpath, host, portlist):
+
+	host = host.split(".")[0]
 
 	for port in portlist:
 
-		subprocess.Popen([mpath + '/mongod', '--dbpath', mpath+'/data'+port, '--shutdown'])
+		mypath = mpath+'/data_at_'+host+"_"+port
+		subprocess.Popen([mpath + '/mongod', '--dbpath', mypath, '--shutdown'])
 
 	return
 
+# Posts a single trade entry into database specified
 # t_type is "WANT"/"SELL"
 # t_id is object ID
 # username contains information for direct connection to poster of trade
@@ -52,6 +61,7 @@ def post_trade (mpath, host, port, t_type, t_id, username):
 
 	return
 
+# Finds trade entry based on given object id t_id
 def find_trade (mpath, host, port, t_id):
 
 	command = "var myCursor = db.trades.find({object: '"+t_id+"'}); \
@@ -81,59 +91,50 @@ def show_collections (mpath, host, port):
 
 	return
 
-
-# Location of mongod program
-mpath = "/home/accts/km637/mongodb-linux-i686-3.0.1/bin"
-# Local host name
-localhost = socket.gethostname()
-
-build_rings (mpath, portlist)
-
-# maybe later fix so that read pipe, resume when rings are set up
-time.sleep(1)
-
 ####################################################
-# Example instance
 
-portlist = ["27017","27018"]
+def main(argv):
 
-bunchfile = [("turtle", "27017")]
+	# Location of mongod program
+	mpath = "/home/accts/km637/mongodb-linux-i686-3.0.1/bin"
+	# Local host name
+	localhost = socket.gethostname()
 
-# post_trade (mpath, "", portlist, "HAVE", "123", "Koji")
-
-for i in range(10):
-	find_trade(mpath, localhost, "27017", "123")
-
-# 	show_collections(mpath, "", "27017")
-
-time.sleep(1)
-
-kill_rings (mpath, portlist)
-
-# out = subprocess.check_output([mpath + "/mongo", "--eval", "db.adminCommand('listDatabases')"])
-# print out*10
-
-# out = subprocess.check_output([mongodpath + '/mongo', '--eval', 'printjson(db.getCollectionNames())'])
-# print out*10
-# print "\n"*10
-
-# time.sleep(1)
+	portlist = ["27017","27018"]
+	bunchfile = [("chameleon.zoo.cs.yale.edu", "27017"),("ladybug.zoo.cs.yale.edu", "27017")]
 
 
-def ask (trade):
+	build_rings (mpath, localhost, portlist)
+
+	# maybe later fix so that read pipe, resume when rings are set up
+	time.sleep(1)
+
+	# clear_collection (mpath, localhost, "27017", "trades")
+
+	# post_trade (mpath, "", portlist, "HAVE", "123", "Koji")
 
 
-	return
+	for i in range(10):
+		find_trade(mpath, localhost, "27017", "123")
+
+	# 	show_collections(mpath, "", "27017")
+
+	time.sleep(1)
+
+	inp = raw_input("quit: ")
+
+	if inp == "y":
+		kill_rings (mpath, localhost, portlist)
 
 
-def bid (trade):
+	# out = subprocess.check_output([mpath + "/mongo", "--eval", "db.adminCommand('listDatabases')"])
+	# print out*10
 
+	# out = subprocess.check_output([mongodpath + '/mongo', '--eval', 'printjson(db.getCollectionNames())'])
+	# print out*10
+	# print "\n"*10
 
-	return
+	# time.sleep(1)
 
-
-def check_sales ():
-
-
-	return
-
+if __name__ == "__main__":
+    main(sys.argv[1:])
