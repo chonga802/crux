d6300979461f7edabd00cd9b81574d1da9d009ab
Sage Price <sage.price@yale.edu>
2015-04-26 17:04:40 -0400

    Configure clusters now functional, updated to use bunchfiles stored locally in bunches folder and save clusters to local clusters folder as <nodename>_cluster.txt.

--- configure_clusters.py
+++ configure_clusters.py
@@ -13,31 +13,38 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-nodes')
 args = parser.parse_args()
 nodes = args.nodes
+
+#read in nodes
 n = open(nodes,'r')
+nodes=[]
+for l in n:
+    nodes.append(l.rstrip('\n'))
+n.close()
 
 subprocess.call(["rm", "-r", "clusters"])
 subprocess.call(["mkdir", "clusters"])
 
 # for each node
-for node in n:
+for node in nodes:
     print "configuring cluster for " + node
-    node = node.rstrip('\n')
     ncluster = node + "_cluster.txt"
     cluster = []
     # for each bunchfile
-    for target in n:
-        print "target is " + target
-        target = target.rstrip('\n')
-        nodef = "pl_pings/" + target + "_file.txt"
-        f = open(nodef, 'r')
-        for line in f.splitlines():
+    for target in nodes:
+        if node != target:
+            print " target is " + target
+            bunchf = "bunches/" + target + "_bunch.txt"
+            with open(bunchf, 'r') as f:
+                for line in f:
             # if the file contains a line with our node,
             # copy it into our cluster list
-            if node in line:
-                cluster.append(line)
+                    if node in line:
+                        words = line.split()
+                        cluster.append((target,words[1]))
     # sort based on ping time
+    print cluster
     cluster.sort(key=lambda data: Decimal(data[1]))
-    f = open("clusters/" + ncluster, 'w')
+    with open("clusters/" + ncluster, 'w') as f:
     # save and close
-    f.write(cluster)
-    f.close()
+        strout ='\n'.join('%s %s' % x for x in cluster)+'\n' 
+        f.write(strout)
